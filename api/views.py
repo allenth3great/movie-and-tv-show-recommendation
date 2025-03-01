ea495f68f525d8135916a223cf4ec9e47c1a5376
allenth3great <lspascual@student.hau.edu.ph>
2025-03-01 15:48:00 +0800

    (#25) feat:Initialize project

---
+++ api/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
+from .services import get_tokens_for_user
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({"message": "User  registered successfully."}, status=status.HTTP_201_CREATED)
+
+class LoginView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            tokens = get_tokens_for_user(user)
+            return Response(tokens, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
